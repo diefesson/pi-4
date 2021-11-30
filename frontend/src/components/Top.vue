18dcc04b4e837c63765aa41dd22fdbc69c8737b4
diefesson <diefesson.so@gmail.com>
2021-11-30 10:59:15 -0300

    feat: home page

---
+++ frontend/src/components/Top.vue
@@ -0,0 +1,49 @@
+<template>
+  <header class="top">
+    <span v-if="!signed">Criar conta</span>
+    <span v-if="signed">Sair</span>
+    <span>|</span>
+    <span v-if="!signed">Entrar</span>
+    <span v-if="signed">{{ username }}</span>
+    <div class="profile"></div>
+  </header>
+</template>
+
+<style scoped>
+.top {
+  box-sizing: border-box;
+  width: 100%;
+  padding: 5px;
+  gap: 10px;
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-end;
+  align-items: center;
+}
+
+.top > span {
+  font-size: 14px;
+  font-family: "Roboto", sans-serif;
+}
+
+.profile {
+  width: 30px;
+  height: 30px;
+  border-radius: 100%;
+  background-color: darkgray;
+}
+</style>
+
+<script>
+// TODO: use data from backend
+// TODO: add actions
+
+export default {
+  data: () => {
+    return {
+      signed: true,
+      username: "diefesson",
+    };
+  },
+};
+</script>
