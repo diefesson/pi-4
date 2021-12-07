edb79670d243381d5ee6fb0d426b6691fd6d51d6
Natanael Vieira <natanael.vieira.barboza@gmail.com>
2021-12-06 22:17:22 -0300

    feat: criado novas entidades
    criado entidade Answers e Statistcs

---
+++ backend/src/entities/Answers.ts
@@ -0,0 +1,31 @@
+import { Column,ManyToOne,JoinColumn, Entity, PrimaryColumn } from "typeorm";
+import {Questions} from "./Questions";
+
+@Entity("answers")
+class Answers{
+    @PrimaryColumn()
+    id: number;
+
+    @Column()
+    description: string;
+
+    @Column()
+    isCorrect: boolean;
+
+    @Column()
+    question_id: boolean;
+
+    @ManyToOne(()=> Questions)
+    @JoinColumn({name: "question_id"})
+    question: Questions;
+
+    @Column()
+    created_at: Date;   
+
+    @Column()
+    updated_at: Date;   
+    
+    @Column()
+    deleted_at: Date;
+
+}
