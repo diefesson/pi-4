9c28833264bcaca935fa295ed53ce34136593faa
Natanael Vieira <natanael.vieira.barboza@gmail.com>
2021-12-06 23:46:19 -0300

    chore: adicionado descrição da porta

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,6 +2,6 @@ import { app } from "./app";
 
 
 app.listen(3000, () => {
-    console.log("Server is runner! \nWelcome the Game! ");
+    console.log("Server is runner in the port 3000! \nWelcome the Game! ");
 });
 
