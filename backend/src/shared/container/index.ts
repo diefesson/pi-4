ebbba12b8384caf6f978c167d95cbd327d4eaf47
Natanael Vieira <natanael.vieira.barboza@gmail.com>
2021-12-08 21:56:13 -0300

    feat: adicionado singleton para questionRepository e AnswersRepository

--- backend/src/shared/container/index.ts
+++ backend/src/shared/container/index.ts
@@ -1,4 +1,10 @@
 import { container } from 'tsyringe';
+import { AnswersRepository } from '../../repositories/AnswersRepository';
 import { PlayersRepository } from '../../repositories/PlayersRepository';
+import { QuestionsRepository } from '../../repositories/QuestionsRepository';
 
 container.registerSingleton<PlayersRepository>('PlayersRepository', PlayersRepository);
+
+container.registerSingleton<QuestionsRepository>('QuestionsRepository', QuestionsRepository);
+
+container.registerSingleton<AnswersRepository>('AnswersRepository', AnswersRepository);
