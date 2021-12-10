472875669d495b22e3dfa46718cbbe9edeb9f292
Natanael Vieira <natanael.vieira.barboza@gmail.com>
2021-12-09 21:49:57 -0300

    refactor: refatorado as rotas de questions

--- backend/src/routes/questions.routes.ts
+++ backend/src/routes/questions.routes.ts
@@ -1,19 +1,25 @@
 import {Router} from "express";
 import {QuestionsController} from "../controller/QuestionsController";
+import { ensureAdmin } from "../middlewares/ensureAdmin";
 
 var questionsRoutes = Router();
 
 const questionsController = new QuestionsController();
-questionsRoutes.post("/", questionsController.create);
-questionsRoutes.get("/{id}", questionsController.get);
-questionsRoutes.get("/{id}/answers/{answers_id}/isCorrect", questionsController.get);
-// pegar questoes e respostas
-// verificar se uma questão esta certa
-// pegar todas as questoes de um usuário
-// pegar todas as questoes e respostas (player deve ser Admin)
 
-// atualizar questoes e perguntas
-// deletar questões 
+questionsRoutes.post("/", questionsController.create);
+
+questionsRoutes.get("/",
+    //ensureAdmin,
+    questionsController.getAll); 
+
+questionsRoutes.get("/:id", questionsController.get);
+
+questionsRoutes.get("/:id/answers/:answers_id/isCorrect", questionsController.answersIsCorrect);
+
+questionsRoutes.put("/:id", questionsController.put);
+
+questionsRoutes.delete("/:id", questionsController.delete);
 
 export {questionsRoutes}
+
 
