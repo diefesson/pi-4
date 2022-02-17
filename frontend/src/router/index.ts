850714f3d22f592a3d92dba22d98a83efdc516e2
diefesson <diefesson.so@gmail.com>
2022-02-17 20:16:12 -0300

    feat(frontend): login guard

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -7,6 +7,7 @@ import PauseView from "@/views/PauseView.vue";
 import ListQuestionsView from "@/views/ListQuestionsView.vue";
 import AddQuestionView from "@/views/AddQuestionView.vue";
 import Game from "@/views/GameView.vue";
+import { authService } from "@/service";
 
 const routes = [
   {
@@ -18,11 +19,17 @@ const routes = [
     path: "/list-questions",
     name: "List questions",
     component: ListQuestionsView,
+    meta: {
+      requiresAuth: true,
+    },
   },
   {
     path: "/add-question",
     name: "Add question",
     component: AddQuestionView,
+    meta: {
+      requiresAuth: true,
+    },
   },
   {
     path: "/login",
@@ -39,23 +46,35 @@ const routes = [
     name: "Correct",
     component: PauseView,
     props: true,
+    meta: {
+      requiresAuth: true,
+    },
   },
   {
     path: "/pause/:pauseStatus",
     name: "Incorrect",
     component: PauseView,
     props: true,
+    meta: {
+      requiresAuth: true,
+    },
   },
   {
     path: "/pause/:pauseStatus",
     name: "stop",
     component: PauseView,
     props: true,
+    meta: {
+      requiresAuth: true,
+    },
   },
   {
     path: "/game",
     name: "game",
     component: Game,
+    meta: {
+      requiresAuth: true,
+    },
   },
   {
     path: "/:pathMatch(.*)*",
@@ -66,6 +85,18 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some((record) => record.meta.requiresAuth)) {
+    if (!authService.isAuthenticated()) {
+      next({ name: "Home" });
+    } else {
+      next();
+    }
+  } else {
+    next();
+  }
 });
 
 export default router;
